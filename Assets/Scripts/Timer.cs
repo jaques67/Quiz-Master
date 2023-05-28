a1a1b00c0b1339417a7624aff344576ab8783777
Jaques Strydom <jaques.strydom@gmail.com>
2023-05-28 07:42:21 +0200

    Calculate Timer image fill value
    Refactored code to be the same as the tutorial. Will change it back
    later
    Calculate a fillFraction value between 0 and 1 which will be used to
    show the remaining time in our timer image
    Created a variable to inform us to load the next question
    Created a method to cancel the timer early for when the user answered
    the question.

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -7,7 +7,9 @@ public class Timer : MonoBehaviour
     [SerializeField] float timeToCompleteQuestion = 30f;
     [SerializeField] float timeToShowCorrectAnswer = 10f;
 
+    public bool loadNextQuestion;
     public bool isAnsweringQuestion = false;
+    public float fillFraction;
 
     float timerValue;
 
@@ -16,24 +18,43 @@ public class Timer : MonoBehaviour
         UpdateTimer();
     }
 
+    public void CancelTimer()
+    {
+        timerValue = 0;
+    }
+
     void UpdateTimer()
     {
         timerValue -= Time.deltaTime;
 
-        if (timerValue <= 0)
+        // changed to be same as tutorial
+        if (isAnsweringQuestion)
         {
-            isAnsweringQuestion = !isAnsweringQuestion;
-            if (isAnsweringQuestion )
+            if (timerValue > 0)
             {
-                timerValue = timeToCompleteQuestion;
+                fillFraction = timerValue / timeToCompleteQuestion;
             }
             else
-            {
+            { 
+                isAnsweringQuestion = false;
                 timerValue = timeToShowCorrectAnswer;
+
             }
-            
+        }
+        else
+        {
+            if (timerValue > 0)
+            {
+                fillFraction = timerValue / timeToShowCorrectAnswer;
+            }
+            else
+            { 
+                isAnsweringQuestion = true;
+                timerValue = timeToCompleteQuestion;
+                loadNextQuestion = true;
+            }
         }
 
-        Debug.Log(timerValue);
+        Debug.Log($"{ isAnsweringQuestion }: { timerValue } = { fillFraction }");
     }
 }
